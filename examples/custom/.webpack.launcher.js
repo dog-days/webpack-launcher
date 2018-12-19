b7069838360a18846a7c88861eb90446af531f1d
xianshannan <xianshannan@qq.com>
2018-12-19 18:38:54 +0800

    update example custom

--- examples/custom/.webpack.launcher.js
+++ examples/custom/.webpack.launcher.js
@@ -2,8 +2,18 @@
 const path = require('path');
 
 module.exports = {
+  host: 'webpack.launcher.com',
   proxy: {
     '/cigweb/v1/': 'http://cig.whtest.topvdn.com:57165/',
+    '/v3/assistant/': {
+      target: 'https://restapi.amap.com/',
+      // https 不要做安全检测 false
+      secure: false,
+      // 高德做了域名限制
+      // It is useful in some cases like using name-based virtual hosted sites.
+      // https://en.wikipedia.org/wiki/Virtual_hosting#Name-based
+      changeOrigin: true,
+    },
   },
   alias: {
     src: path.resolve('./src'),
