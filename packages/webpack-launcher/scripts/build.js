1621f9857a805e5eff6dafb7c801bec41eb203dd
xianshannan <xianshannan@qq.com>
2018-12-20 14:08:13 +0800

    add node version check

--- packages/webpack-launcher/scripts/build.js
+++ packages/webpack-launcher/scripts/build.js
@@ -2,6 +2,8 @@
 // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/scripts/build.js
 
 'use strict';
+// 检测 node 版本
+require('webpack-launcher-utils/checkNodeVersion')();
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
