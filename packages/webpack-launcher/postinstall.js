9ae25c9b216841b523198885bd98fec67b45b351
xianshannan <xianshannan@qq.com>
2018-12-23 13:20:50 +0800

    Add postinstall to copy public folder

--- packages/webpack-launcher/postinstall.js
+++ packages/webpack-launcher/postinstall.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 // 如果上一个目录是 node_modules，则执行命令
@@ -8,6 +8,9 @@ const nodeModulewebpackLauncherPackageJson = path.resolve(
   '../../node_modules/webpack-launcher/package.json'
 );
 if (fs.existsSync(nodeModulewebpackLauncherPackageJson)) {
+  const copyedPublicFolder = path.resolve('public');
+  fs.ensureDirSync(copyedPublicFolder);
+  fs.copySync(path.resolve(__dirname, './template/public'), copyedPublicFolder);
   // node_modules 上一级文件夹下的 package.json
   const pacakgeJsonPath = path.resolve(__dirname, '../../package.json');
   const pacakgeJson = require(pacakgeJsonPath);
