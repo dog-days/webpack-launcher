4ae3304c42af0c8e9228b4921b97c149bc420be1
xianshannan <xianshannan@qq.com>
2018-12-24 13:22:11 +0800

    Add the removed mark code in webpackLauncher.config.js in ejection

--- packages/webpack-launcher/config/webpackLauncher.config.js
+++ packages/webpack-launcher/config/webpackLauncher.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const webpackLauncherConfig = require('webpack-launcher-utils/webpackLauncherConfig');
 
+// @remove-on-eject-begin
 if (!webpackLauncherConfig.appPublic || !fs.existsSync(webpackLauncherConfig.appPublic)) {
   // 无 appPublic，且 appPublic 路径不存在，即使用者项目根目录找不到 appPublic 目录
   // 那么使用默认的 appPublic 路径
@@ -13,4 +14,5 @@ if (!webpackLauncherConfig.appHtml || !fs.existsSync(webpackLauncherConfig.appHt
   // 那么使用默认的 appHtml 路径
   webpackLauncherConfig.appHtml = path.resolve(__dirname, './../template/public/index.html');
 }
+// @remove-on-eject-end
 module.exports = webpackLauncherConfig;
