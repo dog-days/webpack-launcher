36d0085be8814f894155cd0dcb1368a41b97e7d5
xianshannan <xianshannan@qq.com>
2018-12-24 15:33:14 +0800

    update example custom

--- examples/custom/mock/.mock.config.js
+++ examples/custom/mock/.mock.config.js
@@ -1,21 +1,21 @@
 'use strict';
 
 module.exports = function(mockApp) {
-  mockApp.get('keeper/v1/topic/:id', function(req, res) {
+  mockApp.get('/keeper/v1/topic', function(req, res) {
     res.send({ url: req.url, query: req.query });
   });
-  mockApp.post('keeper/v1/topic/:id', function(req, res) {
+  mockApp.post('/keeper/v1/topic', function(req, res) {
     res.send({ url: req.url, query: req.query });
   });
-  mockApp.post('keeper/v1/topic/add/:id', function(req, res) {
-    res.send({ url: req.url, query: req.query, body: req.body });
-  });
-  mockApp.patch('keeper/v1/topic/edit/:id', function(req, res) {
+  mockApp.patch('/keeper/v1/topic/:id', function(req, res) {
     res.send({
       url: req.url,
       query: req.query,
       body: req.body,
       'data|10': [{ 'id|+1': 1 }],
     });
+  });
+  mockApp.delete('/keeper/v1/topic/:id', function(req, res) {
+    res.send({ url: req.url, query: req.query });
   });
 };
