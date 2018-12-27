c7f5aad0fb34b54fb368c9dd339701b827831132
xianshannan <xianshannan@qq.com>
2018-12-27 16:05:37 +0800

    Fix decorators not work problem, use the legacy stage 1

--- packages/webpack-launcher/config/babel.config.js
+++ packages/webpack-launcher/config/babel.config.js
@@ -24,15 +24,18 @@ module.exports = {
     // like Internet Explorer, will require both the promise and
     // iterator polyfills be added manually.
     '@babel/plugin-syntax-dynamic-import',
+    // 这个是 es7 提案，并不是标准，用法如下
+    // 这里使用 legacy (stage 1) decorators 语法和行为。
+    // @decorator
+    // class Test {}
+    // 当 legacy = true 时，需要用在 `@babel/plugin-proposal-class-properties`
+    // 之前，否则无效
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     // 使用 class properties 新用法
     // class Test {
     //   displayName = 'test';
     // }
     '@babel/plugin-proposal-class-properties',
-    // 这个是 es7 提案，并不是标准，用法如下
-    // @decorator
-    // class Test {}
-    ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
     // 需要配套安装 @babel/runtime
     // 其中 helper 功能可以减少转换生成的代码（通过引用 @bable/runtime）
     '@babel/plugin-transform-runtime',
