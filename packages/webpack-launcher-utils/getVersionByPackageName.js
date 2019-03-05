512e27ca44702166583486c9360a3a6f80245e1b
xianshannan <xianshannan@qq.com>
2019-03-05 14:25:52 +0800

    feat: change the package version source
    we get version from the node_module package

--- packages/webpack-launcher-utils/getVersionByPackageName.js
+++ packages/webpack-launcher-utils/getVersionByPackageName.js
@@ -1,16 +1,29 @@
-const path = require('path');
+const fs = require('fs');
 const chalk = require('chalk');
 
 /**
  * 根据包名获取 node_modules 中包版本
+ * @return { String } packageName npm 包名，可以是 xxx、xxx/xxx/xxx、@xxx/xxxx、@xxx/xxx/xxx
  * @return { String } version
  */
 module.exports = function getVersionByPackageName(packageName) {
-  const cwdPackageJson = require(path.resolve('package.json'));
-  const allDependencies = { ...cwdPackageJson.dependencies, ...cwdPackageJson.devDependencies };
-  const version = allDependencies[packageName];
+  let packageJsonPath;
+  const packageNameSplit = packageName.split('/');
 
-  if (!version) {
+  if (!!~packageName.indexOf('@') && packageNameSplit.length > 2) {
+    // pacakgeName 为 @xxx/xxx/xxx 需要取出 @xxx/xxx 真正的包名
+    const [groupFolderName, packageFolderName] = packageNameSplit;
+
+    packageJsonPath = require.resolve(`${groupFolderName}/${packageFolderName}/package.json`);
+  } else if (packageNameSplit.length > 1) {
+    const [packageFolderName] = packageNameSplit;
+
+    packageJsonPath = require.resolve(`${packageFolderName}/package.json`);
+  } else {
+    packageJsonPath = require.resolve(`${packageName}/package.json`);
+  }
+
+  if (!fs.existsSync(packageJsonPath)) {
     console.error(
       chalk.red(
         `The package ${packageName} is not found in the pacakge.json. Run ${chalk.cyan(
@@ -20,6 +33,8 @@ module.exports = function getVersionByPackageName(packageName) {
     );
     process.exit(1);
   }
+
+  const version = require(packageJsonPath).version;
 
   return version;
 };
