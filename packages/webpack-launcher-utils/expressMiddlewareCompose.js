5b73ee07c68d3fbc8e9f0b6cf96395471573fe0b
xianshannan <xianshannan@qq.com>
2019-03-18 15:44:07 +0800

    chore: update comment

--- packages/webpack-launcher-utils/expressMiddlewareCompose.js
+++ packages/webpack-launcher-utils/expressMiddlewareCompose.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * 从左到右合并 express middleware
+ * 从左到右合并 express middleware，避免回调地狱
  * middlewares 的执行顺序从左到右，这个跟 redux 的 compose 的顺序相反（因为 next 回调）
- * @param {array} middlewares The express middlewares
+ * @param {Array} middlewares The express middlewares
  * @returns {Function} 返回一个组合的 express middleware
  * 例如 compose(f, g, h) 会组合为：
  * (req,res,next) => f(req,res,g(req,res,h(req,res,next)))
@@ -17,7 +17,7 @@ function compose(middlewares) {
   if (middlewares.length === 1) {
     return middlewares[0];
   }
-  // arrow function 返回 "{}"，可读性更好
+  // 这个逻辑需要一点思考，arrow function 可读性不好
   return middlewares.reduce(function(a, b) {
     return function(req, res, next) {
       return a(req, res, function() {
