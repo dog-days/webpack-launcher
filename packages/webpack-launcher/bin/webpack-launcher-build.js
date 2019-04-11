94088044bb9720d2627d027d612f98e1d935c18b
xianshannan <xianshannan@qq.com>
2019-04-11 17:07:57 +0800

    fix: the webpack launcher config process.env.NODE_ENV undefined problem

--- packages/webpack-launcher/bin/webpack-launcher-build.js
+++ packages/webpack-launcher/bin/webpack-launcher-build.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
+// 因为 webpackLauncher.config 需要用到，所以要放在最前面
+process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = 'development';
+
 const { appDllBuild, dllEntry } = require('../config/webpackLauncher.config');
 const { shouldBuildDll } = require('webpack-launcher-utils/dllEntryUtils');
 
